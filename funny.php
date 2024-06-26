df29ccb98441453cb2d83ab9925300f57c15bf34
7TE <140772499+7TE2@users.noreply.github.com>
2024-06-25 20:38:35 -0400

    Update and rename get_gif.php to funny.php

---
+++ funny.php
@@ -0,0 +1,9 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$escaped_ip = escapeshellarg($ip);
+$output_file = tempnam(sys_get_temp_dir(), 'ipgif_') . '.gif';
+shell_exec("python3 generate_gif.py $escaped_ip $output_file");
+header('Content-Type: image/gif');
+readfile($output_file);
+unlink($output_file);
+?>
--- get_gif.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$ip = $_SERVER['REMOTE_ADDR'];
-echo json_encode(['ip' => $ip]);
-?>
